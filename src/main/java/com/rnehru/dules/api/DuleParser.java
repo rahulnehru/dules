9e9687ee780f8b7f895837019c56e3e7730c40c4
rahul.nehru <rnehru@deloitte.co.uk>
2019-07-09 10:51:27 +0100

    parser api

--- src/main/java/com/rnehru/dules/api/DuleParser.java
+++ src/main/java/com/rnehru/dules/api/DuleParser.java
@@ -1,9 +1,21 @@
 package com.rnehru.dules.api;
 
+import com.rnehru.dules.parser.DSLParser;
+import com.rnehru.dules.rule.Rule;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 class DuleParser {
 
+    private DSLParser parser = new DSLParser();
 
-//    public Rule parse(String ruleRep) {
-//        return parser.parse(ruleRep);
-//    }
+    public Rule parseRule(String dslRule) {
+        return parser.parse(dslRule);
+    }
+
+    public List<Rule> parseRules(List<String> dslRules) {
+        return dslRules.stream().map(dslRule -> parser.parse(dslRule)).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/rnehru/dules/parser/DSLParser.java
+++ src/main/java/com/rnehru/dules/parser/DSLParser.java
@@ -3,28 +3,17 @@ package com.rnehru.dules.parser;
 import com.rnehru.dules.rule.Rule;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 
 public class DSLParser {
 
-    public static void main(String[] args) {
-        String s = ".page(p__q__>1 & p || p__q__r)";
+    private DulesConcreteVisitor visitor = new DulesConcreteVisitor();
 
-        DulesLexer dl = new DulesLexer(CharStreams.fromString(s));
-
+    public Rule parse(String dslRule) {
+        DulesLexer dl = new DulesLexer(CharStreams.fromString(dslRule));
         CommonTokenStream tokens = new CommonTokenStream(dl);
-
         DulesParser parser = new DulesParser(tokens);
-
-
-        DulesConcreteVisitor visitor = new DulesConcreteVisitor();
-
-        Rule r = visitor.visitHigher(parser.higher());
-
-        System.out.println(r);
-
-
+        return visitor.visitHigher(parser.higher());
     }
+
 }
